e25b8f731e9ae18a151c1f9899b5bc5d63d1885b
SoloGudLyf <solomonteiko2006@gmail.com>
2025-05-14 15:04:21 +0000

    Finished seting up a roud for the game

--- javascript.js
+++ javascript.js
@@ -21,7 +21,7 @@ function getComputerChoice(){
     }
     return computerChoice.toLowerCase();
 }
-console.log(getComputerChoice())
+
 
 // Human selects his choice
 
@@ -33,40 +33,57 @@ function getHumanChoice(){
 // Human vs Computer plays a round.
 
 function playRound(humanChoice, computerChoice){
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
     let winner = "Yah! You won!";
     let loser = "Sorry! You lost";
-    switch (humanChoice, computerChoice){
-        case (humanChoice === computerChoice):
+    let verdict;
+        if (humanChoice === computerChoice){
             let tie = "It's a tie."
-            return tie;
-            break;
+            verdict = `You chose ${humanChoice} and computer chose ${computerChoice} \n ${tie}`
+            return verdict
+        }
         
-        case ((humanChoice === "rock") && (computerChoice === "paper")):
+        else if ((humanChoice === "rock") && (computerChoice === "paper")){
             humanScore += 1;
-            return winner;
-            break;
-        
-        case ((humanChoice === "rock") && (computerChoice === "scissors")):
-            humanScore += 1;
-            return winner;
-            break;
-
-        case ((humanChoice === "paper") && (computerChoice === "rock")):
-            humanScore += 1;
-            return winner;
-            break;
-        
-        case ((humanChoice === "paper") && (computerChoice === "scissors")):
-            computerScore += 1;
-            return loser;
-            break;
-        
-        case ((humanChoice === "scissors") && (computerChoice === "rock")):
-            computerScore += 1;
-            return loser;
-            break;
+            verdict = `You chose ${humanChoice} and computer chose ${computerChoice} \n ${winner}`;
+            return verdict;
             
+            
+        }
+        
+        else if ((humanChoice === "rock") && (computerChoice === "scissors")){
+            humanScore += 1;
+            verdict = `You chose ${humanChoice} and computer chose ${computerChoice} \n ${winner}`;
+            return verdict;
+        }
+
+        else if ((humanChoice === "paper") && (computerChoice === "rock")){
+            humanScore += 1;
+            verdict = `You chose ${humanChoice} and computer chose ${computerChoice} \n ${winner}`;
+            return verdict;
+        }
+        
+        else if ((humanChoice === "paper") && (computerChoice === "scissors")){
+            computerScore += 1;
+            verdict = `You chose ${humanChoice} and computer chose ${computerChoice} \n ${loser}`;
+            return verdict
+        }
+        
+        else if ((humanChoice === "scissors") && (computerChoice === "rock")){
+            computerScore += 1;
+            verdict = `You chose ${humanChoice} and computer chose ${computerChoice} \n ${loser}`;
+            return verdict;
+        }
+        
+        else if ((humanChoice === "scissors") && (computerChoice === "paper")){
+            humanScore += 1;
+            verdict = `You chose ${humanChoice} and computer chose ${computerChoice} \n ${winner}`;
+            return verdict;
+        }
+
     }
-    }
+    
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+
